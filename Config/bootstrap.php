41b182827a12b437f6e6a8a9df5fba03f3da4cc2
Miles Johnson <mileswjohnson@gmail.com>
2012-11-06 23:47:00 -0800

    3.2.0-rc4

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -14,7 +14,7 @@ App::uses('Sanitize', 'Utility');
 /**
  * Current version.
  */
-Configure::write('Forum.version', '3.2.0-rc3');
+Configure::write('Forum.version', '3.2.0-rc4');
 
 /**
  * Name of the User model.
